3a5ec3f9d575ac93292297a055c3929df68aa840
JayampathiBandara <wm.jayampathi@gmail.com>
2020-05-03 20:26:06 +0800

    13.7 Add shared module with exports section

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+//import { CommonModule } from '@angular/common';
 
 import { ProductListComponent } from './product-list.component';
 import { ConvertToSpacesPipe } from '../Shared/convert-to-spaces.pipe';
-import { StarComponent } from '../Shared/star.component';
+//import { StarComponent } from '../Shared/star.component';
 import { ProductDetailComponent } from './product-detail.component';
 
-import { FormsModule } from '@angular/forms';
+//import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
+    //CommonModule,
+    //FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       {
@@ -20,13 +22,13 @@ import { ProductDetailGuard } from './product-detail.guard';
         canActivate: [ProductDetailGuard],
         component: ProductDetailComponent
       }
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,    
-    StarComponent
+    ConvertToSpacesPipe
   ]
 })
 export class ProductModule { }
