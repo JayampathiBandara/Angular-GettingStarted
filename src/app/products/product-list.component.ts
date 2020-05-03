33717dc0f21ce425941155d438f7e36d190f7407
JayampathiBandara <wm.jayampathi@gmail.com>
2020-05-03 12:00:44 +0800

    9.2	Registering services and use lifecycle Hook

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -22,31 +23,17 @@ export class ProductListComponent {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
-    constructor() {
+    products: IProduct[];
+
+    constructor(private productService: ProductService) {
+        //this.filteredProducts = this.products;
+        //Oninit run after constructor. so move above line to onInit
+        //this.filterText = 'cart';
+    }
+
+    ngOnInit(): void {
+        this.products = this.productService.getProducts();
         this.filteredProducts = this.products;
-        this.filterText = 'cart';
     }
 
     performFilter(filterBy: string): IProduct[] {
