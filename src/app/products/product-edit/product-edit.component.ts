4f67fc8b5c81b84d38343dc10c508ad1e407b1fd
jayampathib <wmjb.weerasinghe@gmail.com>
2020-05-05 11:23:19 +0800

    5.2.3 Add routing for editing component

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 import { IProduct } from '../product';
 import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-edit.component.html',
@@ -14,10 +15,34 @@ export class ProductEditComponent {
 
   product: IProduct;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private activRoute: ActivatedRoute,    
+    private router: Router) { }
                
 
-  getProduct(id: number): void {
-
+  ngOnInit(): void {
+    // + use to conver to number
+    let id = +this.activRoute.snapshot.paramMap.get('id');
+    this.getProduct(id);
   }
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.onProductRetrieved(product),
+      error: err => this.errorMessage = err
+    });
+  }
+  onProductRetrieved(product: IProduct): void {
+    this.product = product;
+
+    if (this.product) { 
+      if (this.product.productId === 0) {
+        this.pageTitle = 'Add Product';
+      } else     
+      this.pageTitle = `Product Detail: ${this.product.productName}`;
+    } else
+      this.pageTitle = "No Product Found";
+  }
+  saveProduct(): void {
+  }
+   
 }
